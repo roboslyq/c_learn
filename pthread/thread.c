7451837f991b3cef1f020267c15839781b8964e6
roboslyq <robosloveu@163.com>
2020-04-26 23:52:18 +0800

    updated

--- pthread/thread.c
+++ pthread/thread.c
@@ -4,18 +4,41 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *  async_add(void *args){
-    int i = *(int *)args;
-    printf("args : %d \n" ,i);
+//定义一个结构体类型
+typedef struct {
+    int val1;
+    int val2;
+} ADD_VAL;
+
+int *p;
+
+// 定义线程处理的具体任务
+void *async_add(void *args){
+    ADD_VAL val = *(ADD_VAL *)args;
+    printf("async args : %d  %d \n" ,val.val1,val.val2);
+    printf("thread_id: %d \n ",pthread_self());
+    int res = val.val1 + val.val2;
     return (void*)"OK";
 }
 
+int *p;
 int add(int val1,int val2){
+    ADD_VAL *val;
+    printf("input %d  , %d  \n",val1,val2);
+    printf("thread_id: %d \n ",pthread_self());
+    val -> val1 = val1;
+    val -> val2 = val2;
     pthread_t  pid;
-    int a = 10;
     int  ret;
-    void* cal_res;
-    ret = pthread_create(&pid,NULL,async_add,&a);
-    pthread_join(ret,&cal_res);
-    printf("res: %d \n",*(char *)cal_res);
+    void *retval;
+    ret = pthread_create(&pid,NULL,async_add,val);
+    if(ret<0)
+    {
+        printf("pthread_create error,ret=%d\n",ret);
+        return -1;
+    }
+    pthread_join(pid,&retval);
+    printf("res: %s \n",(char*)retval);
+
+    return retval;
 }
